2296cf6020c0f7f60bab34b9a04c8ff3792e8488
BabyPunk <dhema112@gmail.com>
2022-03-08 09:34:01 +0700

    update notice

--- events/player/error.js
+++ events/player/error.js
@@ -1,6 +1,6 @@
 module.exports = async(queue, error, client) => {
 
-    client.say.queueMessage(client, queue, "An error occurred while playing. Sorry for the inconveniences.", "RED");
+    client.say.queueMessage(client, queue, "Terjadi kesalahan saat bermain. Maaf atas ketidaknyamanannya.", "RED");
 
     return client.utils.sendErrorLog(client, { stack: `${error.message}`, name: "PLAYER_ERROR", code: `${queue.id}` }, "error");
 };
