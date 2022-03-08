497739c90243bf7cff4e345720ddd85c71540179
BabyPunk <dhema112@gmail.com>
2022-03-08 09:36:13 +0700

    Update queueEnd.js

--- events/player/queueEnd.js
+++ events/player/queueEnd.js
@@ -1,5 +1,5 @@
 
 module.exports = async(queue, client) => {
 
-      return client.say.queueMessage(client, queue, "No more songs to play. Left the voice channel.");
+      return client.say.queueMessage(client, queue, "Ga ada musik yang di mainkan lagi. Keluar voice channel.");
 };
